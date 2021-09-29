bf2ee107e4ed6bf27e849e32f7b1e87ba56abd7f
Davy <davy.beauzil72@gmail.com>
2021-09-29 10:12:00 +0200

    feat(page-home): ajout template page accueil

---
+++ wp-content/themes/palmeria-child/page-home.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Template Name: Home page
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Palmeria
+ */
+
+get_header();
+?>
+<div class="pagehome_content">
+    <?php
+    the_content();
+    ?>
+</div>
+
+<?php
+get_footer();
